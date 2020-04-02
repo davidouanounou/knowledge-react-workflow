03d2f8e63810a2a94b39d07fe2b7b3fb34bfa6f0
David ouanounou <david.ouanounou@easy.movie>
2020-04-02 16:38:27 +0200

    Layouts and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Main from './components/layouts/main';
+import Login from './components/layouts/login';
+import LoginHeader from './components/layouts/login-header';
+import LoginBody from './components/layouts/login-body';
+import LoginFooter from './components/layouts/login-footer';
+import Title from './components/atoms/title';
+import InputGroup from './components/atoms/input-group';
+import Label from './components/atoms/label';
+import Input from './components/atoms/input';
+import Button from './components/atoms/button';
 
 function App() {
   return (
-    <div>
-      Hello there !
-    </div>
+    <Main>
+      <Login>
+        <LoginHeader>
+          <Title>Please, log in !</Title>
+        </LoginHeader>
+        <LoginBody>
+          <InputGroup>
+            <Label>Username</Label>
+            <Input placeholder='Type your username' />
+          </InputGroup>
+          <InputGroup>
+            <Label>Password</Label>
+            <Input placeholder='Type your password' type='password' />
+          </InputGroup>
+        </LoginBody>
+        <LoginFooter>
+          <Button>Login</Button>
+        </LoginFooter>
+      </Login>
+    </Main>
   );
 }
 
