ace3c42a12e7e50853dcf805245e54cdfd99d60b
David ouanounou <david.ouanounou@easy.movie>
2020-04-16 11:41:19 +0200

    Storybook

---
+++ src/stories/label.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Label from '../components/atoms/label';
+
+export const Small = () => <Label size='small'>Small label</Label>;
+export const Medium = () => <Label>Medium label</Label>;
+export const Large= () => <Label size='large'>Large label</Label>;
